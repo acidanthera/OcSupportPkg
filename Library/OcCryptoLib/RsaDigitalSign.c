54e98008f654251e0f63584f65c0229f9db26fb8
yourmother <savvamtr@gmail.com>
2019-09-17 20:13:29 +0600

    OcCryptoLib: Add assert according keysize in ModPow

--- Library/OcCryptoLib/RsaDigitalSign.c
+++ Library/OcCryptoLib/RsaDigitalSign.c
@@ -731,6 +731,12 @@ ModPow (
   INT32   Index;
   UINT32  Tmp;
 
+  ASSERT (
+       (Key->Size * sizeof (UINT32) != CONFIG_RSA2048_NUM_BYTES)
+    || (Key->Size * sizeof (UINT32) != CONFIG_RSA4096_NUM_BYTES)
+    || (Key->Size * sizeof (UINT32) != CONFIG_RSA8192_NUM_BYTES)
+      );
+
   A = AllocateZeroPool (Key->Size * sizeof (UINT32));
   Ar = AllocateZeroPool (Key->Size * sizeof (UINT32));
   Aar = AllocateZeroPool (Key->Size * sizeof (UINT32));
